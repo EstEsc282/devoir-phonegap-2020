4738cf28a18ff759f3feb381cd91cf1468419020
EstEsc282 <estebanescudero282@gmail.com>
2020-10-28 04:31:30 +0100

    ajout solution android

--- PhoneGap/modele/Anime.js
+++
@@ -1,8 +0,0 @@
-class Anime{
-  constructor(nom, nbrepisodes, description, id){
-    this.id = id;
-    this.nom = nom;
-    this.nbrepisodes = nbrepisodes;
-    this.description = description;
-  }
-}
